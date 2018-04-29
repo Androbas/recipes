62c6fbe6d27632fd7afc33cc04c4a2c746334e6d
androba <andresrarias@gmail.com>
2018-04-29 18:18:16 -0300

    add delete recipe functionality

---
+++ test/integration/delete_recipe_test_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class DeleteRecipeTestTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = Chef.create!(chefname: "Androba", email: "amdres@mail.com")
+    @recipe = Recipe.create(name:"panchos", description:"hervir salchicas, cortaqr pan por el medio, poner la salchicha entre los panes, agregar condimentos a gusto", chef: @user)
+  end
+
+  test "successfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select "a[href=?]", recipe_path(@recipe), text:"Delete"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+
+end
